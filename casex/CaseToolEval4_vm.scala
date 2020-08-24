3e637ea48b35ef0288649762f911170eccd7ccb4
Jason Belt <belt@ksu.edu>
2020-08-24 21:19:17 +0000

    t

--- casex/CaseToolEval4_vm.scala
+++ casex/CaseToolEval4_vm.scala
@@ -2,18 +2,25 @@
 
 package org.sireum.cli.hamr_runners.casex
 
-import org.sireum.Cli.HamrPlatform
 import org.sireum._
+import org.sireum.Cli.HamrPlatform
 import org.sireum.cli.hamr_runners.{DotFormat, ReadmeGenerator, ReadmeTemplate, Report}
 import org.sireum.hamr.codegen.common.util.ExperimentalOptions
 import org.sireum.message.Reporter
+
+@datatype class Project (simpleName: String,
+                        modelDir: Os.Path,
+                        slangFile: Os.Path,
+                        platforms: ISZ[Cli.HamrPlatform.Type],
+                        shouldSimulate: B,
+                        timeout: Z)
 
 object CaseToolEval4_vm extends App {
 
   val shouldReport: B = T
   val graphFormat: DotFormat.Type = DotFormat.svg
   val build: B = F
-  val timeout: Z = 15000
+  val defTimeout: Z = 15000
 
   val sel4: Cli.HamrPlatform.Type = Cli.HamrPlatform.SeL4
   val sel4_tb: Cli.HamrPlatform.Type = Cli.HamrPlatform.SeL4_TB
@@ -23,14 +30,18 @@ object CaseToolEval4_vm extends App {
 
   var experimentalOptions: ISZ[String] = ISZ(ExperimentalOptions.GENERATE_DOT_GRAPHS)
 
-  def gen(name: String, json: String, platforms: ISZ[Cli.HamrPlatform.Type]): (String, Os.Path, Os.Path, ISZ[Cli.HamrPlatform.Type]) = {
+  def genFull(name: String, json: String, platforms: ISZ[Cli.HamrPlatform.Type], shouldSimulate: B, timeout: Z): Project = {
     val modelDir = case_tool_evaluation_dir / name
     val simpleName = Os.path(name).name // get last dir name
-    return (simpleName, modelDir, modelDir / ".slang" / json, platforms)
+    return Project(simpleName, modelDir, modelDir / ".slang" / json, platforms, shouldSimulate, timeout)
   }
 
-  val tests: ISZ[(String, Os.Path, Os.Path, ISZ[Cli.HamrPlatform.Type])] = ISZ(
-/*
+  def gen(name: String, json: String, platforms: ISZ[Cli.HamrPlatform.Type]): Project = {
+    return genFull(name, json, platforms, T, defTimeout)
+  }
+
+  val nonVmProjects: ISZ[Project] = ISZ(
+
     gen("simple_uav", "UAV_UAV_Impl_Instance.json", ISZ(sel4_tb, sel4_only)),
 
     gen("test_data_port", "test_data_port_top_impl_Instance.json", ISZ(sel4_tb, sel4_only)),
@@ -46,46 +57,45 @@ object CaseToolEval4_vm extends App {
     gen("test_data_port_periodic_domains", "test_data_port_periodic_domains_top_impl_Instance.json", ISZ(sel4_tb, sel4_only, sel4)),
     gen("test_event_data_port_periodic_domains", "test_event_data_port_periodic_domains_top_impl_Instance.json", ISZ(sel4_tb, sel4_only, sel4)),
     gen("test_event_port_periodic_domains", "test_event_port_periodic_domains_top_impl_Instance.json", ISZ(sel4_tb, sel4_only, sel4)),
-
-    // VMs
-    gen("test_data_port_periodic_domains_VM/both_vm", "test_data_port_periodic_domains_top_impl_Instance.json", ISZ(sel4_only, sel4)),
-    gen("test_data_port_periodic_domains_VM/receiver_vm", "test_data_port_periodic_domains_top_impl_Instance.json", ISZ(sel4_only, sel4)),
-    gen("test_data_port_periodic_domains_VM/sender_vm", "test_data_port_periodic_domains_top_impl_Instance.json", ISZ(sel4_only, sel4)),
-
-    gen("test_event_data_port_periodic_domains_VM/both_vm", "test_event_data_port_periodic_domains_top_impl_Instance.json", ISZ(sel4_only, sel4)),
-    gen("test_event_data_port_periodic_domains_VM/receiver_vm", "test_event_data_port_periodic_domains_top_impl_Instance.json", ISZ(sel4_only, sel4)),
-    gen("test_event_data_port_periodic_domains_VM/sender_vm", "test_event_data_port_periodic_domains_top_impl_Instance.json", ISZ(sel4_only, sel4)),
- */
-
-    //gen("test_event_data_port_periodic_domains_VMx/receiver_vm", "test_event_data_port_periodic_domains_top_impl_Instance.json", ISZ(sel4_only)),
-
-    gen("test_event_data_port_periodic_domains_VMx/sender_vm", "test_event_data_port_periodic_domains_top_impl_Instance.json", ISZ(sel4_only)),
-
-    //gen("test_event_data_port_fan_ins_outs_periodic_domains", "test_event_data_port_fan_ins_outs_periodic_domains_top_impl_Instance.json", ISZ(sel4_only))
   )
+
+  val vmProjects: ISZ[Project] = ISZ(
+    // VMs
+    genFull("test_data_port_periodic_domains_VM/both_vm", "test_data_port_periodic_domains_top_impl_Instance.json", ISZ(sel4_only, sel4), F, defTimeout),
+    genFull("test_data_port_periodic_domains_VM/receiver_vm", "test_data_port_periodic_domains_top_impl_Instance.json", ISZ(sel4_only, sel4), F, defTimeout),
+    genFull("test_data_port_periodic_domains_VM/sender_vm", "test_data_port_periodic_domains_top_impl_Instance.json", ISZ(sel4_only, sel4), F, defTimeout),
+
+    genFull("test_event_data_port_periodic_domains_VM/both_vm", "test_event_data_port_periodic_domains_top_impl_Instance.json", ISZ(sel4_only, sel4), F, defTimeout),
+    genFull("test_event_data_port_periodic_domains_VM/receiver_vm", "test_event_data_port_periodic_domains_top_impl_Instance.json", ISZ(sel4_only, sel4), F, defTimeout),
+    genFull("test_event_data_port_periodic_domains_VM/sender_vm", "test_event_data_port_periodic_domains_top_impl_Instance.json", ISZ(sel4_only, sel4), F, defTimeout),
+
+    // VMs with Kent's connector
+    genFull("test_event_data_port_periodic_domains_VMx/receiver_vm", "test_event_data_port_periodic_domains_top_impl_Instance.json", ISZ(sel4_only), F, defTimeout),
+    genFull("test_event_data_port_periodic_domains_VMx/sender_vm", "test_event_data_port_periodic_domains_top_impl_Instance.json", ISZ(sel4_only), F, defTimeout)
+  )
+
+  //val tests: ISZ[Project] = nonVmProjects ++ vmProjects
+  val tests: ISZ[Project] = vmProjects
 
   def run(): Unit = {
     val reporter = Reporter.create
 
     for (project <- tests) {
-      val projectDir = project._2
-      val slangFile = project._3
-
       var reports: HashSMap[Cli.HamrPlatform.Type, Report] = HashSMap.empty
 
-      if(!projectDir.exists) {
-        halt(s"${projectDir} does not exist");
+      if(!project.modelDir.exists) {
+        halt(s"${project.modelDir} does not exist");
       }
-      for (platform <- project._4) {
+      for (platform <- project.platforms) {
 
         println("***************************************")
-        println(s"${projectDir} -- ${platform})")
+        println(s"${project.modelDir} -- ${platform})")
         println("***************************************")
 
         val outputDir: Os.Path = platform match {
-          case Cli.HamrPlatform.SeL4_TB => projectDir / "CAmkES_seL4_TB"
-          case Cli.HamrPlatform.SeL4_Only => projectDir / "CAmkES_seL4_Only"
-          case Cli.HamrPlatform.SeL4 => projectDir / "CAmkES_seL4"
+          case Cli.HamrPlatform.SeL4_TB => project.modelDir / "CAmkES_seL4_TB"
+          case Cli.HamrPlatform.SeL4_Only => project.modelDir / "CAmkES_seL4_Only"
+          case Cli.HamrPlatform.SeL4 => project.modelDir / "CAmkES_seL4"
           case _ => halt("??")
         }
 
@@ -96,7 +106,7 @@ object CaseToolEval4_vm extends App {
           case _ => halt("??")
         }
 
-        if(ops.StringOps(project._2.value).contains("VMx")) {
+        if(ops.StringOps(project.modelDir.value).contains("VMx")) {
           experimentalOptions = experimentalOptions :+ ExperimentalOptions.USE_CASE_CONNECTORS
         }
 
@@ -104,12 +114,12 @@ object CaseToolEval4_vm extends App {
 
         val o = Cli.HamrCodeGenOption(
           help = "",
-          args = ISZ(slangFile.value),
+          args = ISZ(project.slangFile.value),
           json = T,
           verbose = T,
           platform = platform,
 
-          packageName = Some(project._1),
+          packageName = Some(project.simpleName),
           embedArt = T,
           devicesAsThreads = F,
           excludeComponentImpl = T,
@@ -125,7 +135,7 @@ object CaseToolEval4_vm extends App {
 
           camkesOutputDir = Some(camkesOutputDir.value),
           camkesAuxCodeDirs = ISZ(),
-          aadlRootDir = Some(projectDir.value),
+          aadlRootDir = Some(project.modelDir.value),
 
           experimentalOptions = experimentalOptions
         )
@@ -136,11 +146,13 @@ object CaseToolEval4_vm extends App {
           val gen = ReadmeGenerator(o, reporter)
 
           if(gen.build()) {
-            val expectedOutput = gen.simulate(timeout)
+            val expectedOutput: ST =
+              if(project.shouldSimulate) gen.simulate(project.timeout)
+              else st"NEED TO MANUALLY UPDATE EXPECTED OUTPUT"
 
             val report = Report(
               options = o,
-              timeout = timeout,
+              timeout = project.timeout,
               runInstructions = gen.genRunInstructions(case_tool_evaluation_dir),
               expectedOutput = Some(expectedOutput),
               aadlArchDiagram = gen.getAadlArchDiagram(),
@@ -154,8 +166,8 @@ object CaseToolEval4_vm extends App {
       }
 
       if(shouldReport) {
-        val readme = projectDir / "readme_autogen.md"
-        val readmest = ReadmeTemplate.generateReport(project._1, reports)
+        val readme = project.modelDir / "readme_autogen.md"
+        val readmest = ReadmeTemplate.generateReport(project.simpleName, reports)
         readme.writeOver(readmest.render)
       }
     }
