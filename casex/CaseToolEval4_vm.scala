90cc12180592a077eaa67939d190894c9613d97f
Jason Belt <belt@ksu.edu>
2020-10-05 16:40:38 +0000

    t

--- casex/CaseToolEval4_vm.scala
+++ casex/CaseToolEval4_vm.scala
@@ -17,11 +17,12 @@ import org.sireum.message.Reporter
 
 object CaseToolEval4_vm extends App {
 
-  val shouldReport: B = T
+  val shouldReport: B = F
   val graphFormat: DotFormat.Type = DotFormat.svg
   val build: B = F
   val defTimeout: Z = 15000
 
+  val linux: Cli.HamrPlatform.Type = Cli.HamrPlatform.Linux
   val sel4: Cli.HamrPlatform.Type = Cli.HamrPlatform.SeL4
   val sel4_tb: Cli.HamrPlatform.Type = Cli.HamrPlatform.SeL4_TB
   val sel4_only: Cli.HamrPlatform.Type = Cli.HamrPlatform.SeL4_Only
@@ -45,9 +46,9 @@ object CaseToolEval4_vm extends App {
     gen("simple_uav", "UAV_UAV_Impl_Instance.json", ISZ(sel4_tb, sel4_only)),
 
     gen("test_data_port", "test_data_port_top_impl_Instance.json", ISZ(sel4_tb, sel4_only)),
+
     gen("test_data_port_periodic", "test_data_port_periodic_top_impl_Instance.json", ISZ(sel4_tb, sel4_only)),
     gen("test_data_port_periodic_fan_out", "test_data_port_periodic_fan_out_top_impl_Instance.json", ISZ(sel4_tb, sel4_only)),
-
     gen("test_event_data_port", "test_event_data_port_top_impl_Instance.json", ISZ(sel4_tb, sel4_only)),
     gen("test_event_data_port_fan_out", "test_event_data_port_fan_out_top_impl_Instance.json", ISZ(sel4_tb, sel4_only)),
 
@@ -56,6 +57,7 @@ object CaseToolEval4_vm extends App {
 
     gen("test_data_port_periodic_domains", "test_data_port_periodic_domains_top_impl_Instance.json", ISZ(sel4_tb, sel4_only, sel4)),
     gen("test_event_data_port_periodic_domains", "test_event_data_port_periodic_domains_top_impl_Instance.json", ISZ(sel4_tb, sel4_only, sel4)),
+
     gen("test_event_port_periodic_domains", "test_event_port_periodic_domains_top_impl_Instance.json", ISZ(sel4_tb, sel4_only, sel4)),
   )
 
@@ -74,8 +76,9 @@ object CaseToolEval4_vm extends App {
     genFull("test_event_data_port_periodic_domains_VMx/sender_vm", "test_event_data_port_periodic_domains_top_impl_Instance.json", ISZ(sel4_only), F, defTimeout)
   )
 
-  //val tests: ISZ[Project] = nonVmProjects ++ vmProjects
-  val tests: ISZ[Project] = vmProjects
+  //val tests: ISZ[Project] = nonVmProjects
+  val tests: ISZ[Project] = nonVmProjects ++ vmProjects
+  //val tests: ISZ[Project] = vmProjects
 
   def run(): Unit = {
     val reporter = Reporter.create
@@ -96,6 +99,7 @@ object CaseToolEval4_vm extends App {
           case Cli.HamrPlatform.SeL4_TB => project.modelDir / "CAmkES_seL4_TB"
           case Cli.HamrPlatform.SeL4_Only => project.modelDir / "CAmkES_seL4_Only"
           case Cli.HamrPlatform.SeL4 => project.modelDir / "CAmkES_seL4"
+          case Cli.HamrPlatform.Linux => project.modelDir / "Linux"
           case _ => halt("??")
         }
 
@@ -103,6 +107,7 @@ object CaseToolEval4_vm extends App {
           case Cli.HamrPlatform.SeL4_TB => outputDir
           case Cli.HamrPlatform.SeL4_Only => outputDir
           case Cli.HamrPlatform.SeL4 => outputDir / "src/c/CAmkES_seL4"
+          case Cli.HamrPlatform.Linux => outputDir / ""
           case _ => halt("??")
         }
 
@@ -141,6 +146,10 @@ object CaseToolEval4_vm extends App {
         )
 
         org.sireum.cli.HAMR.codeGen(o)
+
+        if(ops.StringOps(outputDir.value).contains("_VM")) {
+          replaceVM()
+        }
 
         if(shouldReport && isSel4(platform)) {
           val gen = ReadmeGenerator(o, reporter)
@@ -202,4 +211,30 @@ object CaseToolEval4_vm extends App {
     }
   }
 
+  def replaceVM(): Unit = {
+
+    val dirs: ISZ[String] = ISZ(
+      "test_data_port_periodic_domains_VM/both_vm",
+      "test_data_port_periodic_domains_VM/receiver_vm",
+      "test_data_port_periodic_domains_VM/sender_vm",
+
+      "test_event_data_port_periodic_domains_VM/both_vm",
+      "test_event_data_port_periodic_domains_VM/receiver_vm",
+      "test_event_data_port_periodic_domains_VM/sender_vm",
+
+      "test_event_data_port_periodic_domains_VMx/receiver_vm",
+      "test_event_data_port_periodic_domains_VMx/sender_vm"
+
+    ).flatMap(m => ISZ(
+      s"${m}/CAmkES_seL4_Only/components/VM/apps",
+      s"${m}/CAmkES_seL4_Only/components/VM/overlay_files"))
+
+    for(d <- dirs) {
+      val path = case_tool_evaluation_dir / d
+      assert(path.exists, s"$path does not exist")
+
+      val comm: ISZ[String] = ISZ("git", "checkout", d)
+      Os.proc(comm).at(case_tool_evaluation_dir).console.runCheck()
+    }
+  }
 }
