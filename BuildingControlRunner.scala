eaadccef0ee8d89b8b7661d530333197c864e006
Jason Belt <belt@ksu.edu>
2020-12-21 19:44:52 +0000

    t

--- BuildingControlRunner.scala
+++ BuildingControlRunner.scala
@@ -3,6 +3,11 @@ package org.sireum.cli.hamr_runners
 import org.sireum._
 import org.sireum.cli._
 
+case class Project (name: String,
+                    json: String,
+                    basePackage: Option[String],
+                    platforms: ISZ[Cli.HamrPlatform.Type])
+
 object BuildingControlRunner {
   val JVM = Cli.HamrPlatform.JVM
   val Linux = Cli.HamrPlatform.Linux
@@ -13,35 +18,39 @@ object BuildingControlRunner {
   def main(args: Array[Predef.String]): Unit = {
     val rootDir = Os.home / "devel/slang-embedded/slang-embedded-building-control"
 
-    val projects: ISZ[(String, String, ISZ[Cli.HamrPlatform.Type])] = ISZ(
-      ("building-control-gen", "BuildingControl_BuildingControlDemo_i_Instance.json",               ISZ(JVM, Linux, MacOS, Cygwin)),
+    var projects: ISZ[Project] = ISZ(
+      Project("building-control-gen", "BuildingControl_BuildingControlDemo_i_Instance.json", None(),ISZ(JVM, Linux, MacOS, Cygwin)),
 
-      ("building-control-gen-alarm-ui", "BuildingControl_BuildingControlDemo_i_Instance.json",      ISZ(JVM)),
+      Project("building-control-gen-alarm-ui", "BuildingControl_BuildingControlDemo_i_Instance.json", None(), ISZ(JVM)),
 
-      ("building-control-gen-mixed-ui", "BuildingControl_BuildingControlDemo_i_Instance.json",          ISZ(JVM)),
+      Project("building-control-gen-mixed-ui", "BuildingControl_BuildingControlDemo_i_Instance.json", None(), ISZ(JVM)),
 
-      ("building-control-gen-shm", "BuildingControl_BuildingControlDemo_i_Instance.json",           ISZ(JVM, Linux, MacOS, Cygwin)),
+      Project("building-control-gen-shm", "BuildingControl_BuildingControlDemo_i_Instance.json", None(), ISZ(JVM, Linux, MacOS, Cygwin)),
 
-      ("building-control-gen-shm-libART", "BuildingControl_BuildingControlDemo_i_Instance.json", ISZ(JVM)),
+      Project("building-control-gen-shm-libART", "BuildingControl_BuildingControlDemo_i_Instance.json", None(), ISZ(JVM)),
 
-      ("building-control-gen-mixed", "BuildingControl_BuildingControlDemo_i_Instance.json",             ISZ(JVM, SeL4, Linux, MacOS, Cygwin)),  // FIXME transpiler issue  
+      Project("building-control-gen-mixed", "BuildingControl_BuildingControlDemo_i_Instance.json", None(), ISZ(JVM, SeL4, Linux, MacOS, Cygwin)),  // FIXME transpiler issue
       
-      ("building-control-gen-periodic", "BuildingControl_periodic_BuildingControlDemo_i_Instance.json", ISZ(JVM, Linux, MacOS, Cygwin)), // FIXME transpiler issue
+      Project("building-control-gen-periodic", "BuildingControl_periodic_BuildingControlDemo_i_Instance.json", None(), ISZ(JVM, Linux, MacOS, Cygwin)), // FIXME transpiler issue
 
       // ("building-control-mixed-excludes", "BuildingControl_BuildingControlDemo_i_Instance.json",    ISZ(JVM, Linux, MacOS, Cygwin)),   // TODO - do we really want to update this one?
     )
 
+    projects = ISZ(
+      Project("building-control-gen-mixed-ui-gd", "BuildingControl_BuildingControlDemo_i_Instance.json", Some("bc"), ISZ(JVM)),
+    )
+
     for (project <- projects) {
-      val projectDir = rootDir / project._1
+      val projectDir = rootDir / project.name
       val cDir = projectDir / "src/c"
       val aadlDir = projectDir / "src/aadl"
-      val slangFile = aadlDir / s".slang/${project._2}"
+      val slangFile = aadlDir / s".slang/${project.json}"
 
       if(!projectDir.exists) {
         throw new RuntimeException(s"${projectDir} does not exist");
       }
 
-      for (platform <- project._3) {
+      for (platform <- project.platforms) {
         val camkesOutputDir: Option[Os.Path] = platform match {
           case Cli.HamrPlatform.SeL4_TB => Some(cDir / "CAmkES_seL4_TB")
           case Cli.HamrPlatform.SeL4_Only => Some(cDir / "CAmkES_seL4_Only")
@@ -51,6 +60,8 @@ object BuildingControlRunner {
 
         val o = Util.o(
           args = ISZ(slangFile.value),
+
+          packageName = project.basePackage,
 
           outputDir = Some(projectDir.value),
 
@@ -63,7 +74,7 @@ object BuildingControlRunner {
           //maxStringSize = 125,
           //maxArraySize = 16,
 
-          embedArt = if(project._1 == string"building-control-gen-shm-libART") F else T,
+          embedArt = if(project.name == string"building-control-gen-shm-libART") F else T,
 
           camkesOutputDir = camkesOutputDir.map(m => m.value),
           camkesAuxCodeDirs = ISZ((cDir / "camkes_aux_code").value),
