4bb9ce4f05084e921f84fb442dfab634098cf4c0
Jason Belt <belt@ksu.edu>
2021-08-19 16:11:54 +0000

    t

--- slash/plugin_install/installOsate.cmd
+++ slash/plugin_install/installOsate.cmd
@@ -25,45 +25,45 @@ val localUpdateSites: B = ops.ISZOps(Os.cliArgs).contains("local")
 
 val sireumHome = Os.path(Os.env("SIREUM_HOME").get)
 
-val osateLoc = Os.home / ".sireum" / "phantom" / "osate-2.9.1-vfinal" / "osate"
+val osateLoc = Os.home / ".sireum" / "phantom" / "osate-2.9.2-vfinal" / "osate"
 val fmideLoc = sireumHome / "bin" / "linux" / "fmide" / "fmide"
 
-val osateExe: Os.Path = if(ops.ISZOps(Os.cliArgs).contains("fmide")) fmideLoc else osateLoc
+val remoteUpdateSite = "https://raw.githubusercontent.com/sireum/osate-update-site/master"
+val localUpdateSite = "file:///home/vagrant/devel/sireum/osate-update-site"
 
-//val osateVer= "2.9.2-vfinal"
-//val OSATE_PRODUCTS_URL=s"https://osate-build.sei.cmu.edu/download/osate/stable/${osateVer}/products/"
+val osateExe: Os.Path = if(ops.ISZOps(Os.cliArgs).contains("fmide")) fmideLoc else osateLoc
 
 val awas = Feature(
   name = "Awas",
   id ="org.sireum.aadl.osate.awas.feature.feature.group",
-  updateSite = "https://raw.githubusercontent.com/sireum/osate-plugin-update-site/master/org.sireum.aadl.osate.awas.update.site",
-  localUpdateSite = Some("file:///home/vagrant/devel/sireum/osate-plugin-update-site/org.sireum.aadl.osate.awas.update.site"))
+  updateSite = remoteUpdateSite,
+  localUpdateSite = Some(localUpdateSite))
 
 val base = Feature(
   name="Base",
-  updateSite = "https://raw.githubusercontent.com/sireum/osate-plugin-update-site/master/org.sireum.aadl.osate.update.site",
-  localUpdateSite = Some("file:///home/vagrant/devel/sireum/osate-plugin-update-site/org.sireum.aadl.osate.update.site"),
+  updateSite = remoteUpdateSite,
+  localUpdateSite = Some(localUpdateSite),
   id = "org.sireum.aadl.osate.feature.feature.group")
 
 val hamr = Feature(
   name="HAMR",
-  updateSite ="https://raw.githubusercontent.com/sireum/hamr-plugin-update-site/master/org.sireum.aadl.osate.hamr.update.site",
-  localUpdateSite = Some("file:///home/vagrant/devel/sireum/hamr-plugin-update-site/org.sireum.aadl.osate.hamr.update.site"),
+  updateSite = remoteUpdateSite,
+  localUpdateSite = Some(localUpdateSite),
   id = "org.sireum.aadl.osate.hamr.feature.feature.group")
 
 val cli = Feature(
   name = "cli",
-  updateSite = "https://raw.githubusercontent.com/sireum/osate-plugin-update-site/master/org.sireum.aadl.osate.update.site",
-  localUpdateSite = Some("file:///home/vagrant/devel/sireum/osate-plugin-update-site/org.sireum.aadl.osate.update.site"),
+  updateSite = remoteUpdateSite,
+  localUpdateSite = Some(localUpdateSite),
   id = "org.sireum.aadl.osate.cli.feature.feature.group")
 
-val order = ISZ(base, hamr, cli)
+val order = ISZ(base, hamr, cli, awas)
 
 for(o <- order) {
   if (isInstalled(o.id, osateExe)) {
     uninstallPlugin(o, osateExe)
   }
-  //installPlugin(o, osateExe)
+  installPlugin(o, osateExe)
 }
 
 println(s"\n\nExecute the following to to launch OSATE :\n")
@@ -79,26 +79,26 @@ println(s"${osateExe}&")
                         localUpdateSite: Option[String])
 
 
-def isInstalled(featureId: String, osateExe: Os.Path): B = {
-  val installedPlugins = Os.proc(ISZ(osateExe.string, "-nosplash", "-console", "-consoleLog", "-application", "org.eclipse.equinox.p2.director",
-    "-listInstalledRoots")).at(osateExe.up).runCheck()
+def isInstalled(featureId: String, _osateExe: Os.Path): B = {
+  val installedPlugins = Os.proc(ISZ(_osateExe.string, "-nosplash", "-console", "-consoleLog", "-application", "org.eclipse.equinox.p2.director",
+    "-listInstalledRoots")).at(_osateExe.up).runCheck()
   return ops.StringOps(installedPlugins.out).contains(featureId)
 }
 
-def uninstallPlugin(feature: Feature, osateExe : Os.Path): Unit = {
+def uninstallPlugin(feature: Feature, _osateExe : Os.Path): Unit = {
   addInfo(s"Uninstalling ${feature.name} OSATE plugin")
-  Os.proc(ISZ(osateExe.string, "-nosplash", "-console", "-consoleLog", "-application", "org.eclipse.equinox.p2.director",
+  Os.proc(ISZ(_osateExe.string, "-nosplash", "-console", "-consoleLog", "-application", "org.eclipse.equinox.p2.director",
     "-uninstallIU", feature.id
-  )).at(osateExe.up).runCheck()
+  )).at(_osateExe.up).runCheck()
 }
 
-def installPlugin(feature: Feature, osateExe : Os.Path): Unit = {
-  val updateSite = if(localUpdateSites) feature.localUpdateSite.get else feature.updateSite
+def installPlugin(feature: Feature, _osateExe : Os.Path): Unit = {
+  val updateSite: String = if(localUpdateSites) feature.localUpdateSite.get else feature.updateSite
   addInfo(s"Installing ${feature.name} OSATE plugin from ${updateSite}")
 
-  Os.proc(ISZ(osateExe.string, "-nosplash", "-console", "-consoleLog", "-application", "org.eclipse.equinox.p2.director",
+  Os.proc(ISZ(_osateExe.string, "-nosplash", "-console", "-consoleLog", "-application", "org.eclipse.equinox.p2.director",
     "-repository", updateSite, "-installIU", feature.id
-  )).at(osateExe.up).runCheck()
+  )).at(_osateExe.up).runCheck()
 }
 
 def addInfo(s: String): Unit = { cprintln(F, s ) }
